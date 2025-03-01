d165901bf4160debe46bd1ed95c50c0020fce75f
vsan61 <70624231+vsan61@users.noreply.github.com>
2025-03-01 21:51:30 +0530

    Add files via upload

---
+++ Area_of_cone.py
@@ -0,0 +1,5 @@
+radius=int(input("enter the radius of the cone: "))
+height=int(input("enter the height of the cone: "))
+pi=3.1452
+area=int(pi*radius*radius*height)
+print(f"Area of cone with radius as {radius} and height as {height} is {area}")
---
+++ data_type_cov.py
@@ -0,0 +1,45 @@
+#int to float
+a = 10
+b = float(a)
+print(type(a))
+print(b)
+print(type(b))
+#int to bool
+c=bool(a)
+print(c)
+print(type(c))
+#int to str
+d=str(a)
+print(d)
+print(type(d))
+
+
+#float to int
+e=45.65
+f=int(e)
+print(f)
+print(type(f))
+#float to bool
+g=bool(e)
+print(g)
+print(type(g))
+#float to str
+h=str(e)
+print(h)
+print(type(h))
+
+
+#str to int
+i="100"
+j=int(i)
+print(j)
+print(type(j))
+#str to bool
+k=bool(i)
+print(k)
+print(type(k))
+#str to float
+l=float(i)
+print(l)
+print(type(l))
+
